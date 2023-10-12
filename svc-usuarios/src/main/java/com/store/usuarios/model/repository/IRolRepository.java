9465bd7ee7778331560e1f17b458a35d7fa2c1e4
César Marcos <39070231+CesarsMarcos@users.noreply.github.com>
2023-10-11 22:30:24 -0500

    cambios en general

--- svc-usuarios/src/main/java/com/store/usuarios/model/repository/IRolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.usuarios.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.usuarios.model.entity.Rol;
-
-public interface IRolRepository extends JpaRepository<Rol, Long>{
-
-}
